90c6cdb00b3b5a62abed8fb101f20d9ffc59301a
chinto <contact@chinto.fr>
2018-07-21 17:04:44 +0200

    Initial commit

---
+++ app/src/androidTest/java/fr/teambrother/exorep/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package fr.teambrother.exorep;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("fr.teambrother.exorep", appContext.getPackageName());
+    }
+}
